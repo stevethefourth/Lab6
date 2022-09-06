44a85baa970ea1ecf248fd5b0f08ea9aa895ccca
samuel ip <samuel.c.ip@student.uts.edu.au>
2022-09-06 19:45:26 +1000

    60%

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -3,7 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TimeManager : MonoBehaviour
-{ 
+{
+    private int lastTime = -1;
+    private float timer;
 
+    public void Start()
+    {
+        resetTime();
+        
+    }
+
+    public void Update()
+    {
+        timer += Time.deltaTime;
+        if ((int)timer > lastTime)
+        {
+            Debug.Log((int)timer);
+            lastTime = (int)timer;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(Time.timeScale == 1.0f)
+            {
+                Time.timeScale = 0.0f;
+            }
+
+            else if(Time.timeScale == 0.0f)
+            {
+                Time.timeScale = 1.0f;
+            }
+
+            Debug.Log("Spacebar pressed");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            resetTime();
+        }
+
+    }
+
+    private void resetTime()
+    {
+        timer = 0;
+        lastTime = -1;
+    }
     
 }
